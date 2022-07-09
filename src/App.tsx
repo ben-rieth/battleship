15257f43917e7d585e7b4f380dcd199df8578def
ben-rieth <benrieth3@gmail.com>
2022-07-09 12:22:15 -0500

    Install and setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Battleship: Coming Soon</h1>
+      <h1 className="text-blue-500 text-3xl">Battleship: Coming Soon</h1>
     </div>
   );
 }
