102b69471fa19b571474e3e045d78516f6d959ac
ben-rieth <benrieth3@gmail.com>
2022-07-09 13:01:37 -0500

    Add state allowing for ship to be hit

--- src/components/ship/Ship.tsx
+++ src/components/ship/Ship.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 type ShipProps = {
     length: number;
 }
 
 const Ship = ({length} : ShipProps) => {
+    const [ship, setShip] = useState<number[]>(Array(length).fill(0))
+
+    const hitShip = (hitIndex: number) => {
+        setShip(ship.map((pos, index) => {
+            if (index === hitIndex) {
+                return -1;
+            }
+            return pos;
+        }))
+    }
+
     return (
         <div data-testid="ship">
-            {Array(length).fill("0").map((_, index) => {
-                return <div key={index} className="w-12 h-12 bg-green-300 outline outline-2 outline-green-700 mt-px ml-px"/>
+            {ship.map((pos, index) => {
+                return (
+                    <div key={index} 
+                        className="w-12 h-12 bg-green-300 outline outline-2 outline-green-700 mt-px ml-px"
+                        onClick={() => hitShip(index)}>
+                        <p>{pos === -1 ? "hit" : ''}</p>  
+                    </div>)
             })}
         </div>
     )
