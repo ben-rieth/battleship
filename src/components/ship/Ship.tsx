2eb70d90abe13f59476afc537cab15036132a666
ben-rieth <benrieth3@gmail.com>
2022-07-09 13:04:57 -0500

    Check to see if ship is sunk

--- src/components/ship/Ship.tsx
+++ src/components/ship/Ship.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type ShipProps = {
     length: number;
@@ -15,6 +15,17 @@ const Ship = ({length} : ShipProps) => {
             return pos;
         }))
     }
+
+    useEffect(() => {
+        const isSunk = () : boolean => {
+            return ship.every((pos) => pos === -1);
+        }
+
+        if(isSunk()) {
+            console.log("Ship Sunk");
+        }
+        
+    }, [ship])
 
     return (
         <div data-testid="ship">
