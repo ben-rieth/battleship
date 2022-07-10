4dcec4334f9586c5091cd86ebbb5ea32130e092a
ben-rieth <benrieth3@gmail.com>
2022-07-10 15:41:39 -0500

    Make ships draggable

--- src/components/ship/Ship.tsx
+++ src/components/ship/Ship.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Rnd } from "react-rnd";
 
 import HitIcon from './../../assets/images/hit.svg';
 
@@ -7,7 +8,7 @@ type ShipProps = {
     color: "red" | "orange" | "yellow" | "green" | "blue";
 }
 
-const Ship = ({length, color} : ShipProps) => {
+const Ship = ({length, color,} : ShipProps) => {
     const [ship, setShip] = useState<number[]>(Array(length).fill(0))
 
     const hitShip = (hitIndex: number) => {
@@ -31,18 +32,22 @@ const Ship = ({length, color} : ShipProps) => {
     }, [ship])
 
     return (
-        <div data-testid="ship">
-            {ship.map((pos, index) => {
-                return (
-                    <div key={index} 
-                        className={`w-12 h-12 mt-px ml-px outline outline-[3px] bg-${color}-300 outline-${color}-700`}
-                        //onClick={() => hitShip(index)}
-                        data-testid="compartment"
-                    >
-                        {pos === -1 ? <img src={HitIcon} alt="hit" /> : ""}
-                    </div>)
-            })}
-        </div>
+        <Rnd enableResizing={false} 
+                dragGrid={[48, 48]} 
+                default={{x: 0, y: 0, width: 'auto', height: 'auto'}}>
+            <div data-testid="ship" className="flex">
+                {ship.map((pos, index) => {
+                    return (
+                        <div key={index} 
+                            className={`w-12 h-12 outline outline-2 bg-${color}-300 outline-${color}-700`}
+                            //onClick={() => hitShip(index)}
+                            data-testid="compartment"
+                        >
+                            {pos === -1 ? <img src={HitIcon} alt="hit" /> : ""}
+                        </div>)
+                })}
+            </div>
+        </Rnd>
     )
 }
 
