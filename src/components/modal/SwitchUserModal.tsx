cbe1e1f7975663267426fdec4d159c5c574ff268
ben-rieth <benrieth3@gmail.com>
2022-07-15 18:26:52 -0500

    Give modal correct width on small screens

--- src/components/modal/SwitchUserModal.tsx
+++ src/components/modal/SwitchUserModal.tsx
@@ -7,7 +7,7 @@ const SwitchUserModal = ({content, isOpen}: SwitchModalProps) => {
 
     return (
         <div className={`fixed z-50 left-0 top-0 w-screen h-screen bg-black/50 ${!isOpen && "hidden"}`}>
-            <div className="flex flex-col gap-3 bg-white rounded w-1/3 mx-auto my-16 p-5">
+            <div className="flex flex-col gap-3 bg-white rounded w-1/2 min-w-fit sm:min-w-0 max-w-xl mx-auto my-16 p-5">
                 {content}
             </div>
         </div>
