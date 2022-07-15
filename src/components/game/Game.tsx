6fa03173c43357010a8bf80d1154b4d20315315d
ben-rieth <benrieth3@gmail.com>
2022-07-15 14:18:37 -0500

    Add basic switching screen between turns

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -6,17 +6,20 @@ type GameMode = "place" | "play";
 
 const Game = () => {
     const [turn, setTurn] = useState<number>(1);
+    const [usersSwitching, setUsersSwitching] = useState<boolean>(false);
     const [mode, setMode] = useState<GameMode>("place");
     const [board1Clickable, setBoard1Clickable] = useState<boolean>(false);
     const [board2Clickable, setBoard2Clickable] = useState<boolean>(false);
     const [boardVisible, setBoardVisible] = useState<1 | 2>(1);
 
+    //switch to play mode after 
     useEffect(() => {
         if(turn > 2) {
             setMode("play");
         }
     }, [turn]);
 
+    //determine when boards are clickable and visible
     useEffect(() => {
         switch(turn) {
             case 1:
@@ -37,12 +40,30 @@ const Game = () => {
         }
     }, [turn]);
 
+    const goToSwitchScreen = () => {
+        setTimeout(() => {
+            setUsersSwitching(true);
+        }, 750);
+        
+    }
+
     const nextTurn = () => {
+        setUsersSwitching(false);
         setTurn(turn + 1);
     }
 
     const switchVisibleBoard = () => {
         setBoardVisible(boardVisible === 1 ? 2 : 1);
+        
+    }
+
+    if(usersSwitching) {
+        return <div>
+            <p>Time to switch players!</p>
+            <p>Click below when you've gotten the device from the other player!</p>
+            <button onClick={nextTurn}>Switch!</button>;
+        </div> 
+        
     }
 
     return (
@@ -52,24 +73,24 @@ const Game = () => {
                     <Switch leftBtnText="Enemy's Board" rightBtnText="Your Board" handleClick={switchVisibleBoard}/>
                 </div>
                 <div className={`flex flex-col items-center gap-5 ${boardVisible !== 1 ? "invisible md:visible order-2" : "order-1"}`}>
-                    <h2 className="font-bold text-3xl">Player 1's Board</h2>
+                    <h2 className="font-bold text-3xl">{turn % 2 === 1 ? "Your Board" : "Enemy's Board"}</h2>
                     <Board id={1} 
                         mode={mode} 
                         showShips={turn % 2 === 1} 
                         canInteract={board1Clickable}
-                        goToNextTurn={nextTurn}/>
+                        goToNextTurn={goToSwitchScreen}/>
                 </div>
                 <div className={`flex flex-col items-center gap-5 ${boardVisible !== 2 ? "invisible md:visible order-2" : "order-1"}`}> 
-                    <h2 className="font-bold text-3xl">Player 2's Board</h2>
+                    <h2 className="font-bold text-3xl">{turn % 2 === 0 ? "Your Board" : "Enemy's Board"}</h2>
                     <Board id={2} 
                         mode={mode} 
                         showShips={turn % 2 === 0} 
                         canInteract={board2Clickable}
-                        goToNextTurn={nextTurn}/>
+                        goToNextTurn={goToSwitchScreen}/>
                 </div>
             </div>
             <button 
-                onClick={nextTurn}
+                onClick={goToSwitchScreen}
                 className={`${mode === "play" && "hidden"} p-3 border-4 border-sky-800 rounded w-1/4 min-w-fit mx-auto`}
             >
                 Done Placing Ships
