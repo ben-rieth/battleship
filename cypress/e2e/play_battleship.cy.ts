f9563e84001663467f7d8910f1f28d8359c3de7b
ben-rieth <benrieth3@gmail.com>
2022-07-16 17:29:23 -0500

    Add e2e test for playing a game with another person

--- cypress/e2e/play_battleship.cy.ts
+++ cypress/e2e/play_battleship.cy.ts
@@ -1,6 +1,6 @@
 import { createYield } from "typescript";
 
-describe('empty spec', () => {
+describe.skip('Test Placement Mode', () => {
   it('allows a player to move their ships in placement mode', () => {
     cy.visit("http://localhost:3000/");
 
@@ -38,4 +38,64 @@ describe('empty spec', () => {
 
     cy.contains('Done Placing Ships').click();
   })
-})
+});
+
+describe("Test Playing Mode", () => {
+  function attackTurn(boardId: number, x: number, y: number, shouldBe: "hit" | "miss") {
+    cy.get(`[data-testid="square-${boardId}-${y}-${x}"]`).click();
+    cy.get('[data-cy="attack-btn"]').click();
+
+    cy.get(`[data-testid="square-${boardId}-${y}-${x}"]`).children('img').should('have.attr', 'alt', shouldBe);
+  }
+
+  it("allows user to play against another human player", () => {
+    cy.visit("http://localhost:3000/");
+
+    //skip setup and positioning ships, ships will be in default positions for this game
+    cy.contains('Done Placing Ships').click();
+    cy.contains('Place Your Ships').click();
+    cy.contains('Done Placing Ships').click();
+    cy.get('[data-cy="attack-btn"]').as('attack').click();
+
+    attackTurn(2, 1, 0, "hit");
+    attackTurn(1, 3, 0, "miss");
+    
+    attackTurn(2, 0, 0, "hit"); //sinks player 2's destroyer
+    attackTurn(1, 7, 8, "hit");
+
+    attackTurn(2, 3, 3, "miss");
+    attackTurn(1, 7, 7, "miss");
+
+    attackTurn(2, 6, 5, "hit");
+    attackTurn(1, 8, 8, "miss");
+
+    attackTurn(2, 6, 6, "hit");
+    attackTurn(1, 6, 8, "hit");
+
+    attackTurn(2, 6, 7, "miss");
+    attackTurn(1, 5, 8, "hit"); //sinks player 1's submarine
+
+    attackTurn(2, 6, 4, "hit");
+    attackTurn(1, 2, 6, "miss" );
+
+    attackTurn(2, 6, 3, "hit");
+    attackTurn(1, 8, 1, "miss");
+
+    attackTurn(2, 6, 2, "hit"); //sinks player 2's carrier
+    attackTurn(1, 0, 8, "hit");
+
+    attackTurn(2, 2, 7, "miss");
+    attackTurn(1, 0, 9, "hit");
+
+    attackTurn(2, 9, 0, "miss");
+    attackTurn(1, 0, 7, "hit");
+
+    attackTurn(2, 8, 6, "miss");
+    attackTurn(1, 0, 6, "hit"); //sinks player 1's battleship
+
+    attackTurn(2, 1, 2, "miss");
+    attackTurn(1, 5, 2, "miss"); 
+
+
+  })
+});
--- src/components/modal/PlayModeModalContent.tsx
+++ src/components/modal/PlayModeModalContent.tsx
@@ -10,7 +10,8 @@ const PlayModeModalContent = ({handleBtnPress} : Props) => {
             <p className="text-center text-lg">If you've just received the device, press the button when you are ready to attack!</p>
             <button 
                 className="border-2 text-2xl font-bold text-red-500 w-fit mx-auto py-2 px-8 rounded-lg hover:bg-red-500 hover:text-white hover:"
-                onClick={handleBtnPress}>Attack!</button>
+                onClick={handleBtnPress}
+                data-cy="attack-btn">Attack!</button>
         </>
     );
 }
