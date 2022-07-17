3c080e84b8b05d1a1af01173712956747ae8cfa0
ben-rieth <benrieth3@gmail.com>
2022-07-16 20:29:21 -0500

    Make sure last log message is always visible

--- src/components/log/Log.tsx
+++ src/components/log/Log.tsx
@@ -7,10 +7,12 @@ type Props = {
 
 const Log = ({messages} : Props) => {
     return (
-        <div className="border-2 border-black mx-auto min-w-fit w-2/3 max-w-2xl px-3 min-h-16 h-16 overflow-y-scroll">
-            {messages.map((message, index) => {
-                return <LogMessage key={`log-${index+1}`} id={index + 1} messageObj={message} />
-            })}
+        <div className="border-2 border-black mx-auto min-w-fit w-2/3 max-w-2xl px-3 min-h-16 h-16 overflow-y-scroll snap-y snap-proximity">
+            <div className="snap-end">
+                {messages.map((message, index) => {
+                    return <LogMessage key={`log-${index+1}`} id={index + 1} messageObj={message} />
+                })}
+            </div>
         </div>
     )
 }
