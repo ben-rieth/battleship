270eb95ed4538fa31df3792b853113324bee3ccf
ben-rieth <benrieth3@gmail.com>
2022-07-18 17:40:27 -0500

    Adjust props on AttackButton component

--- src/components/button/AttackButton.tsx
+++ src/components/button/AttackButton.tsx
@@ -1,17 +1,17 @@
 type Props = {
-    buttonText: string;
     handleClick: () => void;
 }
 
-const AttackButton = ({buttonText, handleClick} : Props) => {
+const AttackButton = ({handleClick} : Props) => {
 
     return (
         <button 
             className={`py-2 px-5 border-4 border-sky-800 rounded-xl min-w-fit mx-auto bg-white text-red-500
                         hover:bg-red-500 hover:text-white`}
             onClick={handleClick}
+            data-cy="attack-btn"
         >
-            <p className={`text-2xl font-semibold`}>{buttonText}</p>
+            <p className={`text-2xl font-semibold`}>Attack!</p>
             
         </button>
     )
