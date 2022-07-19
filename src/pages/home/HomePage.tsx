38bfd2a6e0714ac30236bad59136917926ed7c44
ben-rieth <benrieth3@gmail.com>
2022-07-18 20:23:55 -0500

    Add mode selecting btns to home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,28 @@
+import GameModeButton from "../../components/game_mode_btn/GameModeButton";
+import Header from "../../components/header/Header";
+
+import P2PIcon from './../../assets/images/p2p.svg';
+import SameDeviceIcon from './../../assets/images/inperson.svg';
+import BotIcon from './../../assets/images/robot.svg';
+
 const HomePage = () => {
     return (
         <div>
-            <h1>Home!</h1>
+            <Header />
+            <div className="flex justify-center gap-10">
+                <GameModeButton 
+                    modeName="Play with a Friend" 
+                    subtitle="One Device" 
+                    imgURL={SameDeviceIcon}/>
+                <GameModeButton 
+                    modeName="Play with a Friend" 
+                    subtitle="Peer to Peer" 
+                    imgURL={P2PIcon}/>
+                <GameModeButton 
+                    modeName="Play Against a Bot" 
+                    subtitle="Test Your Skills" 
+                    imgURL={BotIcon}/>
+            </div>
         </div>
     )
 }
